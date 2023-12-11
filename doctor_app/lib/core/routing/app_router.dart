97274c72e4120d6abd3a5a868ac6d7260db1fd28
mobarmeg.9@gmail.com <alzeabiesam@gmail.com>
2023-12-11 16:59:49 +0300

    delete on boarding beacuse i created a new branch for features

--- doctor_app/lib/core/routing/app_router.dart
+++ doctor_app/lib/core/routing/app_router.dart
@@ -1,15 +1,14 @@
 import 'package:doctor_app/core/routing/routes.dart';
 import 'package:doctor_app/features/login/views/login_screen.dart';
-import 'package:doctor_app/features/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case Routes.onBoardingScreen:
-        return MaterialPageRoute(
-          builder: (_) => const OnboardingScreen(),
-        );
+      // case Routes.onBoardingScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const OnboardingScreen(),
+      //   );
       case Routes.loginScreen:
         return MaterialPageRoute(
           builder: (_) => const LoginScreen(),
